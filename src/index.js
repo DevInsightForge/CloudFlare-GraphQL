11b58b407b6fe0b438d3ba9d4161885604538b19
ZèD <imzihad@gmail.com>
2022-06-11 17:47:48 +0600

    Redirect to apollo studio on get request
    Signed-off-by: ZèD <imzihad@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,16 +3,23 @@ import setCors from './utils/setCors';
 
 // Define request handler
 const handleRequest = async (request) => {
-  if (request.method === 'OPTIONS') {
-    return setCors(new Response('', { status: 200 }));
+  const { origin } = new URL(request.url);
+  switch (request.method) {
+    case 'GET':
+      return setCors(new Response('', {
+        // redirect to the GraphQL playground
+        headers: {
+          Location: `https://studio.apollographql.com/sandbox?endpoint=${origin}`,
+        },
+        status: 301,
+      }));
+    case 'OPTIONS':
+      return setCors(new Response('', { status: 200 }));
+    case 'POST':
+      return setCors(await apolloHandler(request));
+    default:
+      return setCors(new Response(`Method ${method} not supported`, { status: 405 }));
   }
-  else if (request.method === 'POST') {
-    return setCors(await apolloHandler(request));
-  }
-  else {
-    return setCors(new Response(`Method ${request.method} not supported`, { status: 405 }));
-  }
-
 }
 
 
