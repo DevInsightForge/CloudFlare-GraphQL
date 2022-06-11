4f8c662f0418b9e6c1ac125ba7a7ea6ed084e2e1
ZèD <imzihad@gmail.com>
2022-06-11 18:06:56 +0600

    Misc changes
    Signed-off-by: ZèD <imzihad@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
 import apolloHandler from './handlers/apolloHandler';
-import setCors from './utils/setCors';
+import setHeaders from './utils/setHeaders';
 
 // Define request handler
 const handleRequest = async (request) => {
   const { origin } = new URL(request.url);
   switch (request.method) {
     case 'GET':
-      return setCors(new Response('', {
-        // redirect to the GraphQL playground
+      return setHeaders(new Response(null, {
         headers: {
           Location: `https://studio.apollographql.com/sandbox?endpoint=${origin}`,
         },
         status: 301,
       }));
     case 'OPTIONS':
-      return setCors(new Response('', { status: 200 }));
+      return setHeaders(new Response(null, { status: 200 }));
     case 'POST':
-      return setCors(await apolloHandler(request));
+      return setHeaders(await apolloHandler(request));
     default:
-      return setCors(new Response(`Method ${method} not supported`, { status: 405 }));
+      return setHeaders(new Response(`Method ${request.method} not supported`, { status: 405 }));
   }
 }
 
--- src/utils/setHeaders.js
+++ src/utils/setHeaders.js
@@ -1,11 +1,12 @@
-const setCorsHeaders = (response) => {
+const setHeaders = (response) => {
     response.headers.set('Access-Control-Allow-Credentials', 'true');
     response.headers.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     response.headers.set('Access-Control-Allow-Methods', 'POST');
     response.headers.set('Access-Control-Allow-Origin', '*');
     response.headers.set('X-Content-Type-Options', 'nosniff');
 
+    // return response with headers
     return response;
 }
 
-export default setCorsHeaders
+export default setHeaders
