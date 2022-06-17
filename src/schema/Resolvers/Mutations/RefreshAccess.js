be2e1bdabd25f593a92111e8114818196b28d2a0
ZèD <imzihad@gmail.com>
2022-06-17 15:21:44 +0600

    Refactored refreshtoken logics
    Signed-off-by: ZèD <imzihad@gmail.com>

--- src/schema/Resolvers/Mutations/RefreshAccess.js
+++ src/schema/Resolvers/Mutations/RefreshAccess.js
@@ -8,41 +8,38 @@ const RefreshAccess = {
   args: {
     refreshToken: { type: GraphQLString },
   },
-  async resolve(parent, { refreshToken }) {
-    if (!refreshToken)
-      throw new AuthenticationError("No refresh token provided");
+  async resolve(_, { refreshToken }) {
+    try {
+      if (!refreshToken) throw new Error("No refresh token provided");
+      const token = refreshToken.split(" ")[1];
+      const isValid = await jwt.verify(token, SECRET);
+      const { payload } = jwt.decode(token);
+      if (!isValid || !payload.email)
+        throw new Error("Provided token is either expired or invalid.");
 
-    const isValid = await jwt.verify(refreshToken, `cgqlJWT`);
-    const { payload } = jwt.decode(refreshToken);
-    if (!isValid || !payload.email)
-      throw new AuthenticationError(
-        "Provided token is either expired or invalid."
+      const { data: user, error } = await pgClient
+        .from("users")
+        .select("*")
+        .eq("email", payload.email.toLowerCase())
+        .single();
+
+      if (error)
+        throw new Error("No user associated with this token. Please sign up.");
+
+      return (
+        "Bearer " +
+        (await jwt.sign(
+          {
+            id: user.id,
+            name: user.name,
+            exp: Math.floor(Date.now() / 1000) + 12 * (60 * 60), // Expires: Now + 12h
+          },
+          SECRET
+        ))
       );
-
-    const {
-      data: { id, name },
-      error,
-    } = await pgClient
-      .from("users")
-      .select("*")
-      .eq("email", payload.email.toLowerCase())
-      .single();
-    if (error)
-      throw new AuthenticationError(
-        "No user associated with this token. Please sign up."
-      );
-
-    return (
-      "Bearer " +
-      (await jwt.sign(
-        {
-          id,
-          name,
-          exp: Math.floor(Date.now() / 1000) + 12 * (60 * 60), // Expires: Now + 12h
-        },
-        SECRET
-      ))
-    );
+    } catch (error) {
+      throw new AuthenticationError(error.message);
+    }
   },
 };
 
