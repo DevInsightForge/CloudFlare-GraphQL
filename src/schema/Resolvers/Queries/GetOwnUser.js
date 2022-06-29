1207fb1e884bf404fdc382d15126e72e61fd171c
ZèD <imzihad@gmail.com>
2022-06-29 10:01:31 +0000

    Do not exhaust db by checking user for each request
    Signed-off-by: GitHub <noreply@github.com>

--- src/schema/Resolvers/Queries/GetOwnUser.js
+++ src/schema/Resolvers/Queries/GetOwnUser.js
@@ -1,11 +1,21 @@
 import { AuthenticationError } from "apollo-server-cloudflare";
+import pgClient from "../../../utils/pgClient";
 import UserType from "../../TypeDefs/UserType";
 
 const GetOwnUser = {
   type: UserType,
   async resolve(_, args, { user, authError }) {
     if (authError) throw new AuthenticationError(authError);
-    return user;
+    const { data: ownUser, error } = await pgClient
+      .from("Users")
+      .select("*")
+      .eq("id", user.id)
+      .single();
+
+    if (error) throw new AuthenticationError("User not found");
+
+    delete ownUser.password;
+    return ownUser;
   },
 };
 
--- src/utils/authCheck.js
+++ src/utils/authCheck.js
@@ -1,25 +1,15 @@
 import jwt from "@tsndr/cloudflare-worker-jwt";
-import pgClient from "./pgClient";
 
 const authCheck = async (request) => {
   const authorization = request.headers.get("authorization");
   try {
     if (!authorization) throw new Error("No authorization header found");
-    const token = authorization.split(" ")[1];
+    const token = authorization.split(" ")[ 1 ];
     const isValid = await jwt.verify(token, SECRET);
     if (!isValid)
       throw new Error("Provided token is either expired or invalid.");
 
-    const { payload } = jwt.decode(token);
-    const { data: user, error } = await pgClient
-      .from("Users")
-      .select("*")
-      .eq("id", payload.id)
-      .single();
-
-    if (error) throw new Error("No authorized user found");
-
-    delete user.password;
+    const { payload: user } = jwt.decode(token);
     return {
       user,
     };
