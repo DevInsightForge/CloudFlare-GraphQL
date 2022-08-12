0ba4ae9575d28e0438e38c8650cde5c0852957c9
ZèD <imzihad@gmail.com>
2022-08-12 15:08:37 -0700

    added new open api to root query

--- src/schema/Resolvers/RootQuery.js
+++ src/schema/Resolvers/RootQuery.js
@@ -1,10 +1,12 @@
 import { GraphQLObjectType } from "graphql";
 import GetAllUsers from "./Queries/GetAllUsers";
+import GetAllOpenUsers from "./Queries/GetAllOpenUsers";
 import GetOwnUser from "./Queries/GetOwnUser";
 
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
+    getAllOpenUsers: GetAllOpenUsers,
     getAllUsers: GetAllUsers,
     getOwnUser: GetOwnUser,
   },
